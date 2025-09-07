23f6054e6917c2926e672f4d61aef71f6fa37d50
Akshay Raju <166707593+deappy@users.noreply.github.com>
2025-09-07 10:49:11 +0530

    added basic calculator features while handling exceptions

--- src/CalculatorApp.java
+++ src/CalculatorApp.java
@@ -1,9 +1,36 @@
 import java.util.Scanner;
+import java.lang.Exception;
+
 class Calculator{
+    
+    public double calculate(double n1,char o,double n2)throws Exception{
+        double res;
+        switch(o){
+            case '+': res = n1 + n2;break;
+            case '-': res = n1 - n2;break;
+            case '*': res = n1 * n2;break;
+            case '%':
+                if(n2==0)throw new ArithmeticException("Division by Zero");
+                res = n1%n2;
+                break;
+            case '/':
+                if(n2==0)throw new ArithmeticException("Division by Zero");
+                res = n1/n2;
+                break;
+            default:
+                throw new Exception("Invalid OPerator!");
+        }
+        return res;
+    }
 
 }
 public class CalculatorApp {
     public static void main(String[] args){
+
+        String bold = "\033[1m";
+        String italic = "\033[3m";
+        String reset = "\033[0m";
+
         Calculator c = new Calculator();
         Scanner sc = new Scanner(System.in);
 
@@ -22,23 +49,36 @@ public class CalculatorApp {
         boolean status = true;
 
         while(status){
+            try{
+                System.out.println("Enter the expression or command");
+                System.out.println("Example 1 : 5 * 2 ");
+                System.out.println("Example 2 : sqrt 5 ");
+                String input = sc.nextLine().trim();
+                
+                if(input.equalsIgnoreCase("quit")){
+                    status = false;
+                    continue;
+                }
 
-            System.out.println("Enter the expression or command");
-            System.out.println("Example 1 : 5 * 2 ");
-            System.out.println("Example 2 : sqrt 5 ");
-            String input = sc.nextLine().trim();
-            
-            if(input.equalsIgnoreCase("quit")){
-                status = false;
-                continue;
-            }
-            if(input.equalsIgnoreCase("history")){
-
+                //basic operations
+                String[] parts = input.split(" ");
+                if(parts.length!=3){
+                    System.out.println("Invalid format!  ");
+                    System.out.println(bold+italic+"(hint: 2 operands and 1 operator with spaces or command and operands with spaces)"+reset+"\n");
+                    continue;
+                }
+                double n1 = Double.parseDouble(parts[0]);
+                double n2 = Double.parseDouble(parts[2]);
+                char o = parts[1].charAt(0);
+                double result = c.calculate(n1, o, n2);
+                System.out.println("Result = "+result+"\n");
+            }catch(Exception e){
+                System.out.println("Error: "+e.getMessage());
             }
 
         }
         
-
-        
+        System.out.println(bold+italic+"Thank you!"+reset);
+        sc.close();
     }
 }
